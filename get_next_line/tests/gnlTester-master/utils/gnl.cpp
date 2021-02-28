732f9be981666b988c5c7a06d3845a867690e3cb
Tomás Ferraz <TomasFerraz@MacBook-Pro-de-Tomas.local>
2021-02-28 18:03:39 -0300

    GNL OK

---
+++ get_next_line/tests/gnlTester-master/utils/gnl.cpp
@@ -0,0 +1,27 @@
+extern "C"
+{
+#define new tripouille
+#include "get_next_line.h"
+#undef new
+}
+
+#include "gnl.hpp"
+#include "color.hpp"
+#include "check.hpp"
+
+#include <string.h>
+
+extern int iTest;
+
+using namespace std;
+
+void gnl(int fd, int r, char const * s)
+{
+	char * line = NULL;
+	int gnlReturn = get_next_line(fd, &line);
+	check(r == gnlReturn && (s == NULL ? line == NULL : !strcmp(line, s)));
+	if (s != NULL)
+		mcheck(line, strlen(s) + 1);
+	free(line);
+	iTest++;
+}
