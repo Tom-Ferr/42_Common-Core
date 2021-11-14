3381f01ee2ec73c96176545033b5b761b55152ad
Tom-Ferr <ferr.wav@gmail.com>
2021-11-13 23:01:11 -0300

    it's over!

--- Inception/srcs/requirements/mariadb/tools/run_db.sh
+++ Inception/srcs/requirements/mariadb/tools/run_db.sh
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    run_db.sh                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tde-cama <tde-cama@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/11/13 22:03:22 by tde-cama          #+#    #+#              #
+#    Updated: 2021/11/13 22:03:23 by tde-cama         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 
 if [ $(ls /var/lib/mysql | wc -l)  -eq 0 ]
 then
@@ -7,7 +19,8 @@ then
     create database $MYSQL_DATABASE;\
     grant all privileges on *.* to '$MYSQL_USER'@'%';\
     flush privileges";
-    mysql -u root -e "alter user 'root'@'localhost' identified by 'secret'";
+    mysql -u root $MYSQL_DATABASE < /wp-db.sql;
+    mysql -u root -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD'";
 fi
 
 exec /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
