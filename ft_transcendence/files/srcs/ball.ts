c6cfd8b750d5155d32400a0047743ce0cabdadc2
Tom-Ferr <ferr.wav@gmail.com>
2022-01-24 15:54:20 -0300

    pong!

---
+++ ft_transcendence/files/srcs/ball.ts
@@ -0,0 +1,81 @@
+import P5 from 'p5'
+import { Paddle } from './paddle'
+export class Ball{
+
+    x: number
+    y: number
+    width: number
+    private height: number
+    xSpeed: number
+    ySpeed: number
+    radius: number
+    velocity: number[]
+
+    constructor(p5: P5, player1: Paddle){
+        this.x = p5.windowWidth / 2
+        this.y = p5.windowHeight / 2
+        this.width = player1.height * 15/100
+        this.height = this.width
+        this.radius = this.width / 2
+        this.start(p5)
+    }
+
+    update = (p5: P5, player1: Paddle, player2: Paddle) => {
+        this.width = player1.height * 15/100
+        if(this.y + this.radius >= p5.windowHeight || this.y - this.radius <= 0){
+            this.ySpeed *= -1
+        }
+        if(this.x - this.radius >= 0 && this.x + this.radius < p5.windowWidth)
+           this.move()
+        else
+            this.reset(p5, player1, player2)
+        if(this.x > player1.x){
+            if(this.x - this.radius <= player1.x + player1.width && this.y >= player1.y && this.y <= player1.y + player1.height ){
+               this.reflect(p5) 
+            }
+
+        }
+        if(this.x < player2.x + player2.width){
+            if(this.x + this.radius >= player2.x && this.y >= player2.y && this.y <= player2.y + player2.height ){
+               this.reflect(p5) 
+            }
+        }
+
+        this.display(p5)
+    }
+
+    display = (p5: P5) => {
+        p5.stroke(255)
+        p5.ellipse(this.x, this.y, this.width)
+    }
+
+    move = () => {
+        this.x += this.xSpeed
+        this.y += this.ySpeed
+    }
+
+    start = (p5: P5) => {
+        var m: number[] = [-1, 1]
+        this.xSpeed = p5.random(p5.windowWidth * 0.5/100, p5.windowWidth * 1.5/100)
+        this.ySpeed = p5.random(p5.windowHeight * 0.25/100, p5.windowHeight * 1.5/100)
+        this.xSpeed *= p5.random(m) 
+        this.ySpeed *= p5.random(m) 
+    }
+
+    reflect = (p5: P5) => {
+        this.xSpeed *= -1 
+        this.xSpeed += p5.random(-1, 2)
+        this.ySpeed += p5.random(-1, 2)
+    }
+
+    reset = (p5: P5, player1: Paddle, player2: Paddle) => {
+        if (this.x - this.radius <= 0)
+            ++player2.score
+        else
+            ++player1.score
+        this.x = p5.windowWidth / 2
+        this.y = p5.windowHeight / 2 
+        this.start(p5)
+    }
+    
+};
---
+++ ft_transcendence/files/srcs/sketch.ts
@@ -0,0 +1,51 @@
+import P5 from 'p5'
+import { Ball } from './ball';
+import { Paddle } from './paddle';
+
+
+const sketch = (p5: P5) => {
+  let player1: Paddle
+  let player2: Paddle
+  let ball: Ball
+  p5.setup = () => {
+    player1 = new Paddle(p5, 5/100)
+    player2 = new Paddle(p5, 95/100)
+    ball = new Ball(p5, player1)
+  }
+  
+  p5.draw = () => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight);
+    p5.background(0)
+    displayLine(p5)
+    displayScore(p5)
+    player1.update(p5)
+    player2.update(p5)
+    ball.update(p5, player1, player2)
+  } 
+
+  p5.keyPressed = () => {
+    if (p5.keyCode == p5.DOWN_ARROW || p5.keyCode == p5.UP_ARROW){
+			player1.isOnMove = true;
+		}
+  }
+
+  p5.keyReleased = () => {
+    if (p5.keyCode == p5.DOWN_ARROW || p5.keyCode == p5.UP_ARROW){
+			player1.isOnMove = false;
+		}
+  }
+
+  function displayLine(p5: P5){
+    p5.stroke(255)
+    p5.line(p5.windowWidth / 2, 0, p5.windowWidth / 2, p5.windowHeight)
+  }
+  function displayScore(p5: P5){
+    p5.stroke(255)
+    p5.textSize(p5.windowWidth * p5.windowHeight * 0.5/10000)
+		p5.fill(255)
+    p5.text(player1.score, p5.windowWidth * 40/100, p5.windowHeight * 7/100)
+    p5.text(player2.score, (p5.windowWidth * 55/100), p5.windowHeight * 7/100)
+  }
+
+};
+let myp5 = new P5(sketch)
